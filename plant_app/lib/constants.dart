d244ca1456ddb056c34065180cd142d15a945f33
Gabriele Scamardo <scamardo.website@gmail.com>
2024-03-08 16:14:11 +0100

    onboarding screen builded

---
+++ plant_app/lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Constants {
+  //Primary color
+  static const primaryColor = const Color(0xff296e48);
+  static const blackColor = Colors.black54;
+
+  //Onboarding texts
+  static const titleOne = "Learn more about plants";
+  static const descriptionOne = "Read how to care for plants in our rich plants guide.";
+  static const titleTwo = "Find a plant lover friend";
+  static const descriptionTwo = "Are you a plant lover? Connect with other plant lovers.";
+  static const titleThree = "Plant a tree, green the Earth";
+  static const descriptionThree = "Find almost all types of plants that you like here.";
+}
